10c56ac67e450100d588894b38ad21eb152df2f9
jrmosedale <J.Mosedale@exeter.ac.uk>
2025-06-06 12:00:38 +0100

    Full bias correction calculation of models using 2020-22 data

--- inst/extdata/data_scripts/calc_biascorrect_models.R
+++ inst/extdata/data_scripts/calc_biascorrect_models.R
@@ -26,12 +26,13 @@ dir_era5<-file.path(dir_root,'era5')# raw 2018, 20,21,22, 24
 dir_era5dly<-file.path(dir_root,'era5')# pre-prep 2010-20
 
 # Dirs for haduk data
-dir_haduk<-file.path(dir_root,'Add_Trees', 'HadUK') # 2018
-dir_hadukcp<-file.path(dir_root,'haduk','haduk_vars')
+dir_haduk1km<-"/Users/jonathanmosedale/Library/CloudStorage/OneDrive-UniversityofExeter/Data/haduk_1km_monthly"
+dir_haduk12km<-"/Users/jonathanmosedale/Data/haduk_12km"
+#dir_hadukcp<-file.path(dir_root,'haduk','haduk_vars')
 
 # UKCP data
 dir_ukcp<- '/Users/jonathanmosedale/Data/mesoclim_inputs' # 2 decades of data 2010-30
-dir_ukcpsst<- '/Users/jonathanmosedale/Data/mesoclim_inputs' # 2019-31
+#dir_ukcpsst<- '/Users/jonathanmosedale/Data/mesoclim_inputs' # 2019-31
 
 # dir for writinng models and data for corretion
 dir_bmodels<-file.path(dir_root,'bias_correct_models')
@@ -42,24 +43,25 @@ era5lsm_file<-file.path(dir_era5,"lsm.tif")
 era5geop_file<-file.path(dir_era5,"geopotential.tif")
 ukcpdtm_file<-file.path(dir_ukcp,'orog_land-rcm_uk_12km_osgb.nc')
 
-# Output defining dtm
-ukdtm_file<-file.path(dir_root,'dtm','uk_dtm.tif') # At output resolution
+# Proportion of land/sea in ukcp 12km grid
+lsm12km.r<-rast("/Users/jonathanmosedale/Library/CloudStorage/OneDrive-UniversityofExeter/Data/Terrain50/uk_seamask_12km.tif")
+plot(lsm12km.r)
 
 
-## Load data
-# ERA5 Landsea mask - convert 0 to NA
-lsm<-rast(era5lsm_file,subds='lsm')
+# ERA5 Landsea mask - convert 0 to NA - proportion of cell that is land
+lsm_era5<-rast(era5lsm_file,subds='lsm')
 
 # ERA5 Elevation (geometric height) from geopotential
-geop<-mask(rast(era5geop_file),ifel(lsm==0,NA,lsm))
-elev<-mesoclimAddTrees::elev_from_geop(geop)
-plot(elev)
+#geop<-mask(rast(era5geop_file),ifel(lsm_era5==0,NA,lsm))
+geop<-rast(era5geop_file)
+elev_era5<-mesoclimAddTrees::elev_from_geop(geop)
+plot(elev_era5)
 
 # UKCP18rcm elevation
 ukcpdtm<-rast(ukcpdtm_file)
 
 #### Years for which historic observed and modelled data will be compared
-years<-c(2017,2018,2019,2020,2021) # respectively among the coldest, wettest, driest, hottest of recent years
+years<-c(2020,2021,2022) # respectively among the coldest 2018, wettest, driest2022, hottest 2012 of recent years
 
 
 #####################################################################
@@ -68,11 +70,7 @@ years<-c(2017,2018,2019,2020,2021) # respectively among the coldest, wettest, dr
 # Remove time from time variable (just date)
 # USES FUNCTION: addtrees_hadukdata
 #####################################################################
-obs_list<-list()
-obs_list$tmax<-rast()
-obs_list$tmin<-rast()
-obs_list$prec<-rast()
-write_yr_tif<-FALSE # TRUE if yearly tifs required
+write_yr_tif<-TRUE # TRUE if yearly tifs required
 
 #download_haduk(dir_haduk,  as.POSIXlt(paste0(years[1],"/01/01")),as.POSIXlt(paste0(years[length(years)],"/12/31")),
 #               c("tasmax","tasmin"),freq = "day", cedausr, cedapwd, access_token)
@@ -83,20 +81,45 @@ for(yr in years){
   enddate<-as.POSIXlt(paste0(yr,"/12/31"))
 
   # Get daily precipitation, tmax and tmin
-  r<-addtrees_hadukdata(startdate, enddate, ukcpdtm, filepath=dir_haduk, var='rainfall')
-  if(write_yr_tif) writeRaster(r,file.path(dir_hadukcp,paste0('rain_',yr,'.tif')),overwrite=TRUE )
-  obs_list$prec<-c(obs_list$prec,r)
 
-  r<-addtrees_hadukdata(startdate, enddate, ukcpdtm, filepath=dir_haduk, var='tasmax')
-  if(write_yr_tif) writeRaster(r,file.path(dir_hadukcp,paste0('tasmax_',yr,'.tif')) )
-  obs_list$tmax<-c(obs_list$tmax,r)
+  r<-import_hadukdata(startdate, enddate, NA, filepath=dir_haduk1km, haduk_res="1km", var = "rainfall")
+  if(write_yr_tif) writeRaster(r,file.path(dir_haduk1km,paste0('rain_',yr,'.tif')),overwrite=TRUE )
 
-  r<-addtrees_hadukdata(startdate, enddate, ukcpdtm, filepath=dir_haduk, var='tasmin')
-  if(write_yr_tif) writeRaster(r,file.path(dir_hadukcp,paste0('tasmin_',yr,'.tif')) )
-  obs_list$tmin<-c(obs_list$tmin,r)
+  r<-import_hadukdata(startdate, enddate, NA, filepath=dir_haduk1km, haduk_res="1km", var = "tasmax")
+  if(write_yr_tif) writeRaster(r,file.path(dir_haduk1km,paste0('tasmax_',yr,'.tif')) )
 
+  r<-import_hadukdata(startdate, enddate, NA, filepath=dir_haduk1km, haduk_res="1km", var = "tasmin")
+  if(write_yr_tif) writeRaster(r,file.path(dir_haduk1km,paste0('tasmin_',yr,'.tif')) )
 }
 
+######## Crop and aggregate to UKCP 12km grid ########
+rain.r<-rast(file.path(dir_haduk1km,paste0('rain_',years,'.tif')))
+rain.r<-crop(extend(rain.r,ext(lsm12km.r)),ext(lsm12km.r))
+rain12km<-terra::aggregate(rain.r,12,fun="mean",na.rm=TRUE)
+rain12km<-mask(rain12km,lsm12km.r, maskvalue=0)
+writeRaster(rain12km,file.path(dir_haduk12km,'rain_12km_3yrs.tif'),overwrite=TRUE)
+
+tasmax.r<-rast(file.path(dir_haduk1km,paste0('tasmax_',years,'.tif')))
+tasmax.r<-crop(extend(tasmax.r,ext(lsm12km.r)),ext(lsm12km.r))
+tmax12km<-terra::aggregate(tasmax.r,12,fun="mean",na.rm=TRUE)
+tmax12km<-mask(tmax12km,lsm12km.r, maskvalue=0)
+writeRaster(tmax12km,file.path(dir_haduk12km,'tmax_12km_3yrs.tif'),overwrite=TRUE)
+
+tasmin.r<-rast(file.path(dir_haduk1km,paste0('tasmin_',years,'.tif')))
+tasmin.r<-crop(extend(tasmin.r,ext(lsm12km.r)),ext(lsm12km.r))
+tmin12km<-terra::aggregate(tasmin.r,12,fun="mean",na.rm=TRUE)
+tmin12km<-mask(tmin12km,lsm12km.r, maskvalue=0)
+writeRaster(tmin12km,file.path(dir_haduk12km,'tmin_12km_3yrs.tif'),overwrite=TRUE)
+
+# Observed list of all climate variables
+obs_list<-list()
+obs_list$tmax<-rast()
+obs_list$tmin<-rast()
+obs_list$prec<-rast()
+
+obs_list$prec<-rain12km
+obs_list$tmax<-tmax12km
+obs_list$tmin<-tmin12km
 
 #####################################################################
 # Get Haduk data to observation list if yearly files already available
@@ -112,7 +135,7 @@ for(yr in years){
 # 1.A. OBSERVED DATA - Get ERA5 hourly variables - convert to daily and resample to UKCP
 # If several years chosen then will create list of spatrasters for each variable for the entire time period
 #####################################################################
-# Variables to get from ERA5 data
+# Variables to get from ERA5 data for bias correction
 vars<-c('psl','huss','hurs','u10','v10','swrad','difrad','lwrad')
 
 ###### Prepare and calculate daily values for each file using .era5todaily and writes daily nc file
@@ -125,7 +148,7 @@ era5day_list<-list()
 t0<-now()
 for (f in era5files){
   print(f)
-  daily<-era5todaily(filein=file.path(dir_era5,f),pathout=dir_era5dly,landsea=lsm,elev=elev,output='singlenc',vars)
+  daily<-era5todaily(filein=file.path(dir_era5,f),pathout=dir_era5dly,landsea=lsm_era5,elev=elev_era5,output='singlenc',vars)
   keys<-unique(c(names(daily),names(era5day_list)))
   if(f==era5files[[1]]) era5day_list<-daily else era5day_list<-mapply(c, era5day_list[keys], daily[keys], SIMPLIFY = FALSE)
   print(now()-t0)
@@ -138,9 +161,7 @@ for(v in names(era5day_list)){
 
 
 ###### Resample daily ERA5 to ukcp resolution ##########################
-#f<-paste0("era5_uk_daily_",startyr,"-",endyr,".nc")
-for (v in vars) obs_list[[v]]<-.resample(era5day_list[[v]],ukcpdtm, msk=TRUE)
-#obs_list$d2m<-.resample(era5day_list$d2m,ukcpdtm, msk=TRUE) # required if recalc rh from satvap
+for (v in vars) obs_list[[v]]<-.resample(era5day_list[[v]],lsm12km.r, msk=TRUE,method="bilinear")
 
 ###### Calculate derived variables ##########################
 # Re-calculate rel humidity? - using d2m, surface pressure and temperature
@@ -159,10 +180,10 @@ for (v in vars) obs_list[[v]]<-.resample(era5day_list[[v]],ukcpdtm, msk=TRUE)
 #rh[rh<20]<-20
 #obs_list$relhum<-.rast(rh,ukcpdtm)
 
-# Calculate wind speed - convert to 2m above ground
+# Calculate wind speed - convert to 2m above ground - KEEP AT 10m
 wsp<-sqrt(obs_list$u10^2+obs_list$v10^2)
-wsp<-.windhgt(wsp, zi=10, zo=2)
 wsp[wsp<0.001]<-0.001
+##wsp<-.windhgt(wsp, zi=10, zo=2)
 obs_list[['windspeed']]<-wsp
 
 # Calculate winddir
@@ -170,63 +191,87 @@ obs_list[['winddir']]<-(atan2(obs_list$u10,obs_list$v10)*(180/pi)+180)%%360
 obs_list<-obs_list[-which(names(obs_list) %in% c('v10','u10'))]
 
 # Change name psl to pres???
+write_climdata(obs_list,filepath=file.path(dir_bcdata,'obs_list_2020_2022.Rds'),overwrite=TRUE)
 
-write_climdata(obs_list,filepath=file.path(dir_bcdata,'obs_list_2017_2022.Rds'),overwrite=FALSE)
+
 
 #####################################################################
 # 2. Get UKCP (model) data
 #####################################################################
+dtmc<-crop(ukcpdtm,lsm12km.r)
+plot(dtmc)
 collection<-'land-rcm'
 domain<-'uk'
 member<-'01'
 rcp<-'rcp85'
-datares<-'12km'
+startdate<-as.POSIXlt(paste0(years[1],'/01/01'))
+enddate<-as.POSIXlt(paste0(years[length(years)],'/12/31'))
 
-#report<-download_ukcp18(dir_ukcp, startdate, enddate,collection = "land-rcm", domain = domain, rcp = rcp, member = member,
-#          vars = c("hurs", "huss", "pr", "prsn", "psl", "rls", "rss", "tasmax", "tasmin", "uas", "vas"), download_dtm = FALSE,
-#          cedausr, cedapwd, access_token)
+climdata<-ukcp18toclimarray(dir_ukcp,dtmc,startdate,enddate,collection,domain,member,temp_hgt=1.5, wind_hgt=10,toArrays=FALSE)
 
-modelled_list<-list()
-
-for (yr in years){
-  startdate<-as.POSIXlt(paste0(yr,"/01/01"))
-  enddate<-as.POSIXlt(paste0(yr,"/12/31"))
-  ukcp18rcm<-ukcp18toclimarray(dir_ukcp, ukcpdtm,  startdate, enddate,
-                             collection, domain, member,wind_hgt=10,toArrays=FALSE)
-  if (yr==years[1]) modelled_list<-ukcp18rcm else modelled_list<-Map(`c`, modelled_list, ukcp18rcm)
-}
 #. ukcp18rcm<-checkinputs(read_climdata(file.path(dir_out,'ukcp18rcm.Rds')), tstep = "day")
 
-#write_climdata(modelled_list,filepath=file.path(dir_bcdata,'modelled_list_2017_2022.Rds'),overwrite=FALSE)
+write_climdata(climdata,filepath=file.path(dir_bcdata,'ukcpdata_2020_2022.Rds'),overwrite=TRUE)
 
 #####################################################################
 # 3. Calculate biascorrect models
 #####################################################################
 
-# Get HadUK mask to define area that can be corrected
-msk<-ifel(is.na(obs_list$tmax[[1]]),NA,1)
-plot(msk)
+# Define area that can be corrected - all UKCP cells that possess land cells from Terrain50 (lsmask12km.r)
+msk<-ifel(lsm12km.r==0,NA,lsm12km.r)
 
 # Get variables and correct names in obs list if required
 vars<-c("relhum","pres","lwrad","swrad","tmax","tmin","windspeed" )
 names(obs_list)[which(names(obs_list)=='psl')]<-"pres"
 names(obs_list)[which(names(obs_list)=='hurs')]<-"relhum"
 vars %in% names(obs_list)
-vars %in% names(modelled_list)
+vars %in% names(climdata)
+
+days<-nlyr(obs_list$tmax)
+days==nlyr(climdata$tmax)
 
 model_list<-list()
 # t0<-now();test1000<-biascorrect(obs_list[[v]],  mask(modelled_list[[v]],msk), mod_out = TRUE, samplenum=1461);now()-t0
 t0<-now()
 for (v in vars) {
   print(v)
-  model_list[[v]]<-biascorrect(obs_list[[v]],  mask(modelled_list[[v]],msk), mod_out = TRUE, samplenum=1461)
+  model_list[[v]]<-biascorrect(mask(obs_list[[v]],msk), hist_mod=mask(climdata[[v]],msk), fut_mod = NA,mod_out = TRUE, rangelims = NA, samplenum=days)
 }
 print(now()-t0)
-# Precipitation
-model_list[["prec"]]<-precipcorrect(obs_list$prec,mask(ukcp18rcm$prec,msk), mod_out = TRUE)
-print(now()-t0)
 
-saveRDS(model_list,file.path(dir_bmodels,paste0('bias_correct_models_2017_2021.Rds')))
+# Precipitation - probably need to check this more re thresholds??
+v<-"prec"
+model_list[[v]]<-precipcorrect(mask(obs_list[[v]],msk), hist_mod=mask(climdata[[v]],msk), mod_out = TRUE)
+
+saveRDS(model_list,file.path(dir_bmodels,paste0('bias_correct_models_2020_2022.Rds')))
+
+#####################################################################
+# 4. Apply biascorrect models
+#####################################################################
+model_list[["prec"]]<-lapply(model_list[["prec"]],unwrap)
+
+# Apply bc models
+bc_list<-list()
+for (v in vars) {
+  bc_list[[v]]<-biascorrect_apply(fut_mod=mask(climdata[[v]],msk), biasmods=model_list[[v]], rangelims = NA)
+
+  dif<-mask(obs_list[[v]],msk)-bc_list[[v]]
+  stats<-c(app(dif,mean),app(dif,sd),(app(mask(obs_list[[v]],msk),max)-app(mask(obs_list[[v]],msk),min))-(app(bc_list[[v]],max)-app(bc_list[[v]],min)),mean(bc_list[[v]]))
+  names(stats)<-c("Mean obs-bc_model","sd obs-bc_model","obs range - bc_model range","Mean bc")
+  plot(stats)
+}
+
+# Apply bc model to precipitation - bit more exploration - decide on prec_threhold =0.01
+v<-"prec"
+bc_list[[v]]<-precipcorrect_apply(fut_mod=mask(climdata[[v]],msk), biasmods=model_list[[v]])
+dif<-mask(obs_list[[v]],msk)-bc_list[[v]]
+stats<-c(app(dif,mean),app(dif,sd),(app(mask(obs_list[[v]],msk),max)-app(mask(obs_list[[v]],msk),min))-(app(bc_list[[v]],max)-app(bc_list[[v]],min)),mean(bc_list[[v]]))
+names(stats)<-c("Mean obs-bc_model","sd obs-bc_model","obs range - bc_model range","Mean bc")
+plot(stats)
+
+
+
+
 
 
 
